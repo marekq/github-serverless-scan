bf85cb0d6b8772377ba906d37d517aafb10fa209
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-07-12 21:27:45 +0200

    added disk_free to dynamodb, bugfixes

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -22,16 +22,22 @@ def get_repo(giturl, gitpath):
 
         # clone the git repo 
         print("git clone " + giturl)
-        gitc = subprocess.Popen("git clone " + giturl + " " + tmppath + " --depth 2", shell = True, stdout = subprocess.PIPE)
+        gitc = subprocess.Popen("git clone " + giturl + " " + tmppath, shell = True, stdout = subprocess.PIPE)
 
         # await for git command to complete
         gitc.communicate()
+
+        total, used, free = shutil.disk_usage(tmppath)
+        disk_free = str(used / (1024.0 ** 2))
+        print(gitpath + " disk used - " + disk_free + " MB")
+        xray_recorder.current_subsegment().put_annotation('disk_usage', disk_free)
+        xray_recorder.current_subsegment().put_annotation('gitrepo', giturl)
 
         # find yaml files
         files = os.listdir(tmppath)
 
         # check content of yaml files
-        for root, dirs, files in os.walk(tmppath, topdown = False):
+        for root, dirs, files in os.walk(tmppath, topdown = True):
             for name in files:
                 if re.search('.yml', name) or re.search('.yaml', name):
 
@@ -45,38 +51,36 @@ def get_repo(giturl, gitpath):
                         yamlfiles.append(fname)
 
                         # scan the yaml file
-                        run_lint(fname, gitpath, name, giturl)
+                        run_lint(fname, gitpath, name, giturl, disk_free)
 
                     else:
                         print("skipping file " + gitpath + " " + fname)
-                    
-        total, used, free = shutil.disk_usage(tmppath)
-        df = str(used / (1024.0 ** 2))
-        print(gitpath + " disk used - " + df + " MB")
-        xray_recorder.current_subsegment().put_annotation('disk_usage', df)
-        xray_recorder.current_subsegment().put_annotation('gitrepo', giturl)
-        
+                     
     return yamlfiles
 
 
 # put ddb record
 @xray_recorder.capture("put_ddb")
-def put_ddb(gitrepo, check_id, check_full):
+def put_ddb(gitrepo, fname, check_id, check_full, check_line, disk_free):
     timest 		= int(time.time())
 
-    ddb.put_item(TableName = os.environ['dynamo_table'], 
-		Item = {
-			'gitrepo'	    : gitrepo,
-            'timest'        : timest,
-			'check_full'    : check_full,
-			'check_id'	    : check_id
-		})
+ddb.put_item(TableName = os.environ['dynamo_table'], 
+    Item = {
+        'gitrepo'	    : gitrepo + "/" + fname + ":" + check_line,
+        'fname'         : fname,
+        'check_line'    : check_line,
+        'timest'        : timest,
+        'check_full'    : check_full,
+        'check_id'	    : check_id,
+        'disk_free'     : disk_free
+    }
+)
 
     print("wrote ddb record for " + gitrepo + " " + check_id)
 
 # run cfn-lint
 @xray_recorder.capture("run_lint")
-def run_lint(yamlfile, gitpath, name, gitrepo):
+def run_lint(yamlfile, gitpath, name, gitrepo, disk_free):
     template, matches = decode.decode(yamlfile, False)
     region = [os.environ['AWS_REGION']]
     count = 0
@@ -91,13 +95,13 @@ def run_lint(yamlfile, gitpath, name, gitrepo):
         )
     
         for check_full in matches:
-            check_id = str(check_full)[1:6]
+            check_id    = str(check_full)[1:6]
+            check_line  = str(check_full).split(":")[-1]
 
-            gitfull = gitrepo + "/" + name
-            put_ddb(gitfull, check_id, str(check_full))
+            put_ddb(gitrepo, name, check_id, str(check_full), check_line, disk_free)
             count += 1
             
-            print(gitpath, name, str(check_full))
+            print(gitrepo + "/" + name + ":" + check_line, name, str(check_full))
 
     except Exception as e:
         print('error reading ' + gitpath + " " + name)
