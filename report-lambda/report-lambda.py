1e97990b2a1074a32fbe5d2deee207c6bca98576
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-08-01 00:05:31 +0200

    added csv output, xray support for report lambda

--- report-lambda/report-lambda.py
+++ report-lambda/report-lambda.py
@@ -1,11 +1,21 @@
-import boto3, os
+import boto3, csv, os
 from boto3.dynamodb.conditions import Key, Attr
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 
+patch_all()
+
+
+# create connection to s3 and dynamodb
 s3 = boto3.client('s3')
 ddb = boto3.resource('dynamodb', region_name = os.environ['AWS_REGION']).Table(os.environ['dynamo_table'])
 
+
+# get s3 bucket name from env var
 s3_bucket = os.environ['s3_bucket']
 
+
+# lambda handler
 def handler(event, context):
 	res = []
 
@@ -29,8 +39,31 @@ def handler(event, context):
 	for x in res:
 		print(x)
 
+	# open the file for writing 
+	filen = open('/tmp/out.csv', 'w') 
+	
+	# create the csv writer object 
+	csv_writer = csv.writer(filen) 
+	
+	# set count to 0 
+	count = 0
+	
+	# iterate over results
+	for x in res: 
+		if count == 0: 
+	
+			# write the csv header
+			csv_writer.writerow(x.keys())
+			count += 1
+	
+		# write the csv record
+		csv_writer.writerow(x.values())
+	
+	# close the file
+	filen.close() 
+
 	# warning - public upload of report file
-	s3.put_object(Bucket = s3_bucket, Body = str(res), Key = scanuuid, ContentType = 'application/json', ACL = 'public-read')
+	s3.put_object(Bucket = s3_bucket, Body = open('/tmp/out.csv', 'rb'), Key = scanuuid + '.csv', ContentType = 'text/csv', ACL = 'public-read')
 
 	# return bucket name and file path
 	return s3_bucket + "/" + str(scanuuid)
