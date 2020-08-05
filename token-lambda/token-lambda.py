6019d9fe91e808689c457878a810fcca54425292
Marek Kuczynski <marek.kuczynski@gmail.com>
2020-08-05 16:56:18 +0200

    added manual input for scan id, added xray support to token-lambda

--- token-lambda/token-lambda.py
+++ token-lambda/token-lambda.py
@@ -1,11 +1,29 @@
 import time
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 
+patch_all()
+
+
+# lambda handler
+@xray_recorder.capture("handler")
 def handler(event, context):
         
-    # generate scan uuid
-    srcuuid = str(hex(int(time.time())))
+    # get the submitted github repo
+    githubrepo = event['GithubRepo']
+    
+    # check if scanid was submitted in step function input
+    if 'ScanID' in event.keys():
+        scan_uuid = str(event['ScanID'])
+        print('received ' + scan_uuid + ' as ScanID input')
 
+    # if scanid not submitted, generate a new scan uuid
+    else:
+        scan_uuid = str(hex(int(time.time())))
+        print('generated ' + scan_uuid)
+
+    # return the githubrepo and scan id
     return {
-        'GithubRepo' : event['GithubRepo'], 
-        'ScanID' : srcuuid
+        'GithubRepo' : githubrepo, 
+        'ScanID' : scan_uuid
     }
